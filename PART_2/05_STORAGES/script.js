5afcb64454aacdf4d13aeff0a5d6036b8535e14a
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-07 19:00:52 +0530

    storage part done

---
+++ PART_2/05_STORAGES/script.js
@@ -0,0 +1,31 @@
+// localstorage
+/*
+    1. how to store
+    localStorage.setItem("name","pratik");
+
+    2. how to data fetch
+    let val = localStorage.getItem("name");
+
+    3.how to remove 
+    localStorage.removeItem("name")
+
+
+    4. how to update
+    localStorage.setItem("name","PRATIK")
+*/
+
+
+// session storage
+// same as local storage systax
+
+// cookies
+// document.cookie = "lang=english"
+
+// const skills = ["HTML", "CSS", "JS"];
+
+// localStorage.setItem("skills", JSON.stringify(skills));
+
+// const storedSkills = JSON.parse(localStorage.getItem("skills"));
+// console.log(storedSkills[0]); 
+
+localStorage.clear();
